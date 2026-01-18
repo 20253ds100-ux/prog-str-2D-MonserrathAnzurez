efb04729e5601cd61a6163dea07c5f7ca1066cc6
Monaserrath <20253ds100@utez.edu.mx>
2026-01-17 20:13:28 -0600

    Agregar proyecto ActividadBuenasPracticas correctamente

--- proyecto-01-hola-mundo/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main{
-    public static void main(String[] args) {
-        System.out.println("Hola Mundo");
-        System.out.println("Monserrath Anzurez Visoso");
-        System.out.println("2D");
-        System.out.println("12/Enero/2026");
-    }
-}
