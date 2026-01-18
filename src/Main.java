9b8648508a0fadf39b5e74ffa198a18f735047db
Monaserrath <20253ds100@utez.edu.mx>
2026-01-17 19:54:10 -0600

    Se arregla el código de malas prácticas

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,21 @@
-
 import java.util.Scanner;
-public class Main{
-    public static void main(String[]a){
-        Scanner sc=new Scanner(System.in);
-        System.out.print("n:");int n=sc.nextInt();
-        int s=0;
-        for(int i=1;i<=n;i++){s+=i;}
-        System.out.println("r:"+s);
-    }}
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
-
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int numero = pedirEntero(sc);
+        int resultado = sumarHastaN(numero);
+        System.out.println("La suma de los números del 1 al " + numero + " es: " + resultado);
+        sc.close();
+    }
+    public static int pedirEntero(Scanner scanner) {
+        System.out.print("Ingrese un número (Por favor que sea entero): ");
+        return scanner.nextInt();
+    }
+    public static int sumarHastaN(int numero) {
+        int suma = 0;
+        for (int i = 1; i <= numero; i++) {
+            suma += i;
+        }
+        return suma;
     }
 }
