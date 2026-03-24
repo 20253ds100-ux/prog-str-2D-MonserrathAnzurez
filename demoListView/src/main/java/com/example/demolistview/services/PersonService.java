0f61b0b6058812cba716900c18cedce04e16a548
Monaserrath <20253ds100@utez.edu.mx>
2026-03-23 20:43:59 -0600

    guardar cambios

--- demoListView/src/main/java/com/example/demolistview/services/PersonService.java
+++ demoListView/src/main/java/com/example/demolistview/services/PersonService.java
@@ -1,10 +1,16 @@
 package com.example.demolistview.services;
+
 import com.example.demolistview.repositories.PersonFileRepository;
+
 import java.io.IOException;
+import java.security.PublicKey;
 import java.util.ArrayList;
 import java.util.List;
+
 public class PersonService {
+
     private PersonFileRepository repo = new PersonFileRepository();
+
     public List<String> loadDataforList() throws IOException {
         List<String> lines = repo.readAllLines();//Recupera las lineas del archivo
         List<String> result = new ArrayList<>();//El listado de resultado con el formato deseado
@@ -21,12 +27,39 @@ public class PersonService {
         }
         return result;
     }
+
     public void addPerson(String name, String email, int edad) throws IOException {
         validatePerson(name, email, edad);
         String nameNoComa= name.replace(",", "");
         String emailNoComa= email.replace(",", "");
         repo.appendNewLine(nameNoComa+","+emailNoComa+ "," + edad);
     }
+
+    public void updatePerson(int index, String name, String email, String edad) throws IOException {
+        List<String> lines = getAllCleanLines();
+        if(index == -1){
+            throw new IllegalArgumentException("El indice es invalido");
+        }
+        lines.set(index, name+","+email+","+edad);
+        repo.appendAllLines(lines);
+    }
+
+    public void deletePerson(int index) throws IOException {
+        List<String> lines = getAllCleanLines();
+        lines.remove(index);
+        repo.appendAllLines(lines);
+    }
+
+    private List<String> getAllCleanLines() throws IOException {
+        List<String> lines=repo.readAllLines();
+        List<String> cleanLines = new ArrayList<>();
+        for(String line : lines){
+            if(line!=null && !line.isBlank()){
+                cleanLines.add(line);
+            }
+        }return cleanLines;
+    }
+
     private void validatePerson(String name, String email, int edad) {
         if (name==null || name.isBlank() || name.length() < 3) {
             throw new IllegalArgumentException("El nombre no cumple con los estandares");
